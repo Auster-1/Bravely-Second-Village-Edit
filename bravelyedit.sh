47dd78a5301b2acfa6d1f413ca0e0b419cabfab1
Auster-1 <115106336+Auster-1@users.noreply.github.com>
2024-08-06 01:11:45 -0300

    Update bravelyedit.sh

--- bravelyedit.sh
+++ bravelyedit.sh
@@ -2,17 +2,14 @@
 
 #READS SAVE FILE YOU'D LIKE TO EDIT
 echo "What is the save file you'd like to edit?"
-echo "(enter 1, 2, or 3)"
+echo "(enter 0, 1, or 2)"
 read -r savefile
-if [ "$savefile" -eq 1 ]; then
-  savepath="$HOME/.local/share/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY0.sav"
-  #savepath="$HOME/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY0.sav"
+if [ "$savefile" -eq 0 ]; then
+  savepath="BD2COLONY0.sav"
+elif [ "$savefile" -eq 1 ]; then
+  savepath="BD2COLONY1.sav"
 elif [ "$savefile" -eq 2 ]; then
-  savepath="$HOME/.local/share/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY1.sav"
-  #savepath="$HOME/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY1.sav"
-elif [ "$savefile" -eq 3 ]; then
-  savepath="$HOME/.local/share/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY2.sav"
-  #savepath="$HOME/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY2.sav"
+  savepath="BD2COLONY2.sav"
 else
   echo "That is not a known save file." && exit
 fi
@@ -23,14 +20,14 @@ if [ ! -f "$savepath" ]; then echo "Couldn't find $savepath." && exit; fi
 #FETCHES CURRENT POPULATION COUNT
 prehexcount=$(xxd -p -s 4 -l 1 "$savepath")
 predeccount=$(echo "ibase=16; $prehexcount" | bc)
-echo "Norende Village currently has $predeccount residents."
+echo "The Village currently has $predeccount residents."
 
 #READS THE DESIRED POPULATION COUNT IN DECIMAL
-echo "How many Norende Village residents would you like?"
+echo "How many Village residents would you like?"
 echo "(enter an integer between 0 and 256)"
 read -r deccount
 if [ "$deccount" -lt 1 ]; then
-  echo "Norende's population cannot be lower than 1." && exit
+  echo "The Village's population cannot be lower than 1." && exit
 elif [ "$deccount" -gt 255 ]; then
   echo "This script does not support populations greater than 255." && exit
 fi
@@ -40,10 +37,5 @@ hexcount=$(printf "%02X" "$deccount")
 
 #VRITES HEXADECIMAL TO SAVE FILE
 printf '%s' "$hexcount" | xxd -r -p | dd of="$savepath" bs=1 seek=4 count=1 conv=notrunc status=none
-echo "Norende Village now has $deccount residents."
+echo "The Village now has $deccount residents."
 
-
-#LINUX CITRA BRAVELY DEFAULT COLONY SAVE LOCATION
-#/home/user/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY0.sav
-#/home/user/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY1.sav
-#/home/user/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000/000fc500/data/00000001/COLONY2.sav
